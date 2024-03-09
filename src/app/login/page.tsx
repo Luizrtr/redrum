3c5d040454ebe04513743852552170cc50c6c421
Luiz Eduardo <luizrtr@outlook.com>
2024-03-09 10:28:17 -0300

    Defined Component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
 import Switch from "@/components/Switch";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import * as React from "react";
 
 export default function Login() {
   return (
     <section className="flex flex-col md:flex-row h-screen items-center">
-      <div className="bg-primary-indigo-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
+      <div className="bg-indigo-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
         <img
           src="https://source.unsplash.com/random"
           alt="img "
@@ -14,55 +17,41 @@ export default function Login() {
       </div>
 
       <div
-        className="bg-primary-gray-300 dark:bg-dark w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
+        className="bg-gray-300 dark:bg-dark w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
         flex items-center justify-center"
       >
         <div className="w-full h-100">
-          <h1 className="text-xl md:text-2xl font-bold leading-tight mt-12 text-primary-gray-900 dark:text-primary-gray-300">
+          <h1 className="text-xl md:text-2xl font-bold leading-tight mt-12 text-gray-900 dark:text-gray-300">
             Login
           </h1>
 
           <form className="mt-6" action="#" method="POST">
-            <div>
-              <label className="block text-primary-gray-900 dark:text-primary-gray-300">
+            <div className="flex flex-col gap-4">
+              <Label className="block text-gray-900 dark:text-gray-300">
                 User
-              </label>
-              <input
-                type="user"
-                name=""
-                id=""
-                placeholder="Enter User"
-                className="w-full px-4 py-3 rounded-lg bg-primary-gray-200 dark:bg-primary-gray-900 mt-2 border focus:border-blue-500 focus:bg-primary-white focus:outline-none"
-              />
+              </Label>
+              <Input type="string" placeholder="Enter User" />
             </div>
 
-            <div className="mt-4">
-              <label className="block text-primary-gray-900 dark:text-primary-gray-300">
+            <div className="mt-4 flex flex-col gap-4">
+              <Label className="block text-gray-900 dark:text-gray-300">
                 Password
-              </label>
-              <input
-                type="password"
-                name=""
-                id=""
-                placeholder="Enter Password"
-                className="w-full px-4 py-3 rounded-lg bg-primary-gray-200 dark:bg-primary-gray-900  mt-2 border focus:border-blue-500
-                focus:bg-primary-white focus:outline-none"
-              />
+              </Label>
+              <Input type="password" placeholder="Enter Password" />
             </div>
-
-            <button
+            <Button
               type="submit"
-              className="w-full block bg-primary-indigo hover:bg-primary-indigo-40bg-primary-indigog-indigo-400 text-primary-white font-semibold rounded-lg
+              className="w-full block bg-indigo hover:bg-indigo-40bg-indigog-indigo-400 text-white font-semibold rounded-lg
               px-4 py-3 mt-6"
             >
               Log In
-            </button>
+            </Button>
           </form>
 
-          <hr className="my-6 border-gray-900 dark:border-gray-300 w-full text-primary-gray-900 dark:text-primary-gray-300" />
-          <p className="mt-8 text-primary-gray-900 dark:text-primary-gray-300">
+          <hr className="my-6 border-gray-900 dark:border-gray-300 w-full text-gray-900 dark:text-gray-300" />
+          <p className="mt-8 text-gray-900 dark:text-gray-300">
             Need an account?{" "}
-            <a href="#" className="text-primary-blue font-semibold">
+            <a href="#" className="text-blue font-semibold">
               Create an account
             </a>
           </p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,135 +3,84 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="h-screen w-screen">
-      {/* sidenav */}
-      <div className="fixed left-0 top-0 w-64 h-full bg-[#f8f4f3] p-4 z-50 sidebar-menu transition-transform">
-        <a
-          href="#"
-          className="flex items-center pb-4 border-b border-b-gray-800"
-        >
-          <h2 className="font-bold text-2xl">
-            LOREM{" "}
-            <span className="bg-[#f84525] text-primary-white px-2 rounded-md">
-              IPSUM
-            </span>
-          </h2>
-        </a>
-        <ul className="mt-4">
-          <span className="text-primary-gray-400 font-bold">ADMIN</span>
-          <li className="mb-1 group">
-            <a
-              href=""
-              className="flex font-semibold items-center py-2 px-4 text-primary-gray-900 hover:bg-primary-gray-950 hover:text-primary-gray-100 rounded-md group-[.active]:bg-primary-gray-800 group-[.active]:text-primary-white group-[.selected]:bg-primary-gray-950 group-[.selected]:text-primary-gray-100"
+    <main className="">
+      <div className="navbar bg-base-100 container mx-auto">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="flex-none">
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle"
             >
-              <i className="ri-home-2-line mr-3 text-lg"></i>
-              <span className="text-sm">Dashboard</span>
-            </a>
-          </li>
-          <li className="mb-1 group">
-            <a
-              href=""
-              className="flex font-semibold items-center py-2 px-4 text-primary-gray-900 hover:bg-primary-gray-950 hover:text-primary-gray-100 rounded-md group-[.active]:bg-primary-gray-800 group-[.active]:text-primary-white group-[.selected]:bg-primary-gray-950 group-[.selected]:text-primary-gray-100 sidebar-dropdown-toggle"
-            >
-              <i className="bx bx-user mr-3 text-lg"></i>
-              <span className="text-sm">Users</span>
-              <i className="ri-arrow-right-s-line ml-auto group-[.selected]:rotate-90"></i>
-            </a>
-            <ul className="pl-7 mt-2 hidden group-[.selected]:block">
-              <li className="mb-4">
-                <a
-                  href=""
-                  className="text-primary-gray-900 text-sm flex items-center hover:text-[#f84525] before:contents-[''] before:w-1 before:h-1 before:rounded-full before:bg-primary-gray-300 before:mr-3"
+              <div className="indicator">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
                 >
-                  All
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  />
+                </svg>
+                <span className="badge badge-sm indicator-item">8</span>
+              </div>
+            </div>
+            <div
+              tabIndex={0}
+              className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
+            >
+              <div className="card-body">
+                <span className="font-bold text-lg">8 Items</span>
+                <span className="text-info">Subtotal: $999</span>
+                <div className="card-actions">
+                  <button className="btn btn-primary btn-block">
+                    View cart
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
                 </a>
               </li>
-              <li className="mb-4">
-                <a
-                  href=""
-                  className="text-primary-gray-900 text-sm flex items-center hover:text-[#f84525] before:contents-[''] before:w-1 before:h-1 before:rounded-full before:bg-primary-gray-300 before:mr-3"
-                >
-                  Roles
-                </a>
+              <li>
+                <a>Settings</a>
               </li>
-            </ul>
-          </li>
-          <li className="mb-1 group">
-            <a
-              href=""
-              className="flex font-semibold items-center py-2 px-4 text-primary-gray-900 hover:bg-primary-gray-950 hover:text-primary-gray-100 rounded-md group-[.active]:bg-primary-gray-800 group-[.active]:text-primary-white group-[.selected]:bg-primary-gray-950 group-[.selected]:text-primary-gray-100"
-            >
-              <i className="bx bx-list-ul mr-3 text-lg"></i>
-              <span className="text-sm">Activities</span>
-            </a>
-          </li>
-          <span className="text-primary-gray-400 font-bold">BLOG</span>
-          <li className="mb-1 group">
-            <a
-              href=""
-              className="flex font-semibold items-center py-2 px-4 text-primary-gray-900 hover:bg-primary-gray-950 hover:text-primary-gray-100 rounded-md group-[.active]:bg-primary-gray-800 group-[.active]:text-primary-white group-[.selected]:bg-primary-gray-950 group-[.selected]:text-primary-gray-100 sidebar-dropdown-toggle"
-            >
-              <i className="bx bxl-blogger mr-3 text-lg"></i>
-              <span className="text-sm">Post</span>
-              <i className="ri-arrow-right-s-line ml-auto group-[.selected]:rotate-90"></i>
-            </a>
-            <ul className="pl-7 mt-2 hidden group-[.selected]:block">
-              <li className="mb-4">
-                <a
-                  href=""
-                  className="text-primary-gray-900 text-sm flex items-center hover:text-[#f84525] before:contents-[''] before:w-1 before:h-1 before:rounded-full before:bg-primary-gray-300 before:mr-3"
-                >
-                  All
-                </a>
-              </li>
-              <li className="mb-4">
-                <a
-                  href=""
-                  className="text-primary-gray-900 text-sm flex items-center hover:text-[#f84525] before:contents-[''] before:w-1 before:h-1 before:rounded-full before:bg-primary-gray-300 before:mr-3"
-                >
-                  Categories
-                </a>
+              <li>
+                <a>Logout</a>
               </li>
             </ul>
-          </li>
-          <li className="mb-1 group">
-            <a
-              href=""
-              className="flex font-semibold items-center py-2 px-4 text-primary-gray-900 hover:bg-primary-gray-950 hover:text-primary-gray-100 rounded-md group-[.active]:bg-primary-gray-800 group-[.active]:text-primary-white group-[.selected]:bg-primary-gray-950 group-[.selected]:text-primary-gray-100"
-            >
-              <i className="bx bx-archive mr-3 text-lg"></i>
-              <span className="text-sm">Archive</span>
-            </a>
-          </li>
-          <span className="text-primary-gray-400 font-bold">PERSONAL</span>
-          <li className="mb-1 group">
-            <a
-              href=""
-              className="flex font-semibold items-center py-2 px-4 text-primary-gray-900 hover:bg-primary-gray-950 hover:text-primary-gray-100 rounded-md group-[.active]:bg-primary-gray-800 group-[.active]:text-primary-white group-[.selected]:bg-primary-gray-950 group-[.selected]:text-primary-gray-100"
-            >
-              <i className="bx bx-bell mr-3 text-lg"></i>
-              <span className="text-sm">Notifications</span>
-              <span className=" md:block px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-red-600 bg-red-200 rounded-full">
-                5
-              </span>
-            </a>
-          </li>
-          <li className="mb-1 group">
-            <a
-              href=""
-              className="flex font-semibold items-center py-2 px-4 text-primary-gray-900 hover:bg-primary-gray-950 hover:text-primary-gray-100 rounded-md group-[.active]:bg-primary-gray-800 group-[.active]:text-primary-white group-[.selected]:bg-primary-gray-950 group-[.selected]:text-primary-gray-100"
-            >
-              <i className="bx bx-envelope mr-3 text-lg"></i>
-              <span className="text-sm">Messages</span>
-              <span className=" md:block px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-primary-green-600 text-primary-green-200 rounded-full">
-                2 New
-              </span>
-            </a>
-          </li>
-        </ul>
+          </div>
+        </div>
       </div>
-      <div className="fixed top-0 left-0 w-full h-full bg-primary-gray-50 z-40 md:hidden sidebar-overlay"></div>
-      {/* end sidenav */}
     </main>
   );
 }
