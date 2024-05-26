3511da27c97e978082a71cd6155317e3077dad52
Luiz Eduardo <luizrtr@outlook.com>
2024-05-26 18:13:11 -0300

    feat: api delete service

---
+++ src/app/api/services/delete/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from "next/server";
+import { connectMongoDB } from "@/lib/mongodb";
+import Services from "@/server/models/services";
+
+export async function DELETE(req: NextRequest) {
+  try {
+    const { id } = await req.json();
+    
+    if (!id) {
+      return NextResponse.json(
+        { message: "Service ID is required." },
+        { status: 400 }
+      );
+    }
+  
+    await connectMongoDB();
+    
+    const service = await Services.findById(id);
+    if (!service) {
+      return NextResponse.json(
+        { message: "Service not found." },
+        { status: 404 }
+      );
+    }
+
+    await Services.deleteOne({ _id: id });
+
+    return NextResponse.json(
+      { message: "Service deleted successfully." },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "An error occurred while deleting the service." },
+      { status: 500 }
+    );
+  }
+}
