7711f12d41dc1e3737d0577c00dc910b5f348b22
Eduardo <luizrtr@outlook.com>
2025-03-07 22:41:34 -0300

    Feat: Schema and user

---
+++ src/components/sign-in.tsx
@@ -0,0 +1,33 @@
+
+import { signIn } from "@/lib/auth"
+
+export default function SignIn() {
+  return (
+    <>
+      <form
+        action={async (formData) => {
+          "use server"
+          await signIn("credentials", formData)
+        }}
+      >
+        <label>
+          Email
+          <input name="email" type="email" />
+        </label>
+        <label>
+          Password
+          <input name="password" type="password" />
+        </label>
+        <button>Sign In</button>
+      </form>
+      <form
+        action={async () => {
+          "use server"
+          await signIn("github")
+        }}
+      >
+        <button type="submit">Signin with GitHub</button>
+      </form>
+    </>
+  )
+} 
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,10 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import GitHubProvider from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
+import { signInSchema } from "./zod";
+import { saltAndHashPassword } from "./password"
+import { getUser } from "@/utils/db";
 
 const prisma = new PrismaClient();
 
@@ -11,6 +15,30 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID || "",
       clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+    }),
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        try {
+          let user = null
+
+          const { email, password } = await signInSchema.parseAsync(credentials)
+          const pwHash = saltAndHashPassword(password)
+
+          user = await getUser(email, pwHash)
+   
+          if (!user) {
+            throw new Error("Invalid credentials.")
+          }
+   
+          return user
+        } catch (error) {
+          return error
+        }
+      },
     }),
   ],
 });
